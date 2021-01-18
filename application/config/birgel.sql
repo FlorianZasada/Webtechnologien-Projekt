cf7039fd25327b2f14612d8e29894470c5aa46c8
ofreriks <oliver.freriks@stud.htwk-leipzig.de>
2021-01-18 13:36:35 +0100

    Correct database

--- application/config/birgel.sql
+++ application/config/birgel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 12. Jan 2021 um 15:18
+-- Erstellungszeit: 18. Jan 2021 um 13:35
 -- Server-Version: 10.4.17-MariaDB
 -- PHP-Version: 8.0.0
 
@@ -41,9 +41,34 @@ CREATE TABLE `eventcards` (
 --
 
 INSERT INTO `eventcards` (`id`, `title`, `description`, `image`, `created`, `userid`) VALUES
-(3, 'Burg', 'Besuche die heftige Burg in Birgel', 'Wasserburg_Birgel.jpg', '2021-01-12 13:10:00', 2),
-(9, 'Schickes Haus', 'Ein wirklich schickes Haus', 'Brigel_Haus2.jpg', '2021-01-13 10:14:24', 2),
-(11, 'Wasserrad', 'Birgel hat echt ein Wasserrad?!?', 'Birgel-b2.jpg', '2021-01-14 10:17:04', 2);
+(51, 'sdfsd', 'fsd', 'Birgel-b.jpg', '2021-01-17 21:52:03', 2),
+(53, 'ttgdrfsea', 'esardes', 'Wasserburg_Birgel.jpg', '2021-01-17 21:52:20', 2),
+(54, 'gfhgfhdf', 'dfgfdg', 'Brigel_Haus1.jpg', '2021-01-17 21:52:33', 2),
+(55, 'Test', 'Test', 'Birgel-b1.jpg', '2021-01-17 21:52:53', 2),
+(56, 'fdgdf', 'gdfgdf', 'Wasserburg_Birgel1.jpg', '2021-01-17 21:53:02', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `newscards`
+--
+
+CREATE TABLE `newscards` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `image` varchar(1000) NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `newscards`
+--
+
+INSERT INTO `newscards` (`id`, `title`, `description`, `image`, `created`, `userid`) VALUES
+(1, 'Sack Reis', 'er ist umgefallen', 'Reis.jpg', '2021-01-18 13:15:28', 2),
+(3, 'Burg', 'gdsfsd', 'Birgel-b.jpg', '2021-01-18 13:17:07', 2);
 
 -- --------------------------------------------------------
 
@@ -81,6 +106,13 @@ ALTER TABLE `eventcards`
   ADD KEY `userid` (`userid`);
 
 --
+-- Indizes für die Tabelle `newscards`
+--
+ALTER TABLE `newscards`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userid` (`userid`);
+
+--
 -- Indizes für die Tabelle `users`
 --
 ALTER TABLE `users`
@@ -94,7 +126,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT für Tabelle `eventcards`
 --
 ALTER TABLE `eventcards`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT für Tabelle `newscards`
+--
+ALTER TABLE `newscards`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT für Tabelle `users`
